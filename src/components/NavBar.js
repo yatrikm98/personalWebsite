7c108d10c87dd2a0ce409277dc11cafa596281c7
Yatrik Mehta <yatrikmehta82@gmail.com>
2023-06-12 22:51:19 +0530

    changing resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
                 <li className="p-2 mr-2 cursor-pointer text-indigo-500"><a href="#about">About </a></li>
                 <li className="p-2 mr-2 cursor-pointer text-indigo-500"><a href="#projects"> Projects</a></li>
                 <li className="p-2 mr-2 cursor-pointer text-indigo-500"><a href="#contact"> Contact</a></li>
-                <li className="p-2 mr-48 cursor-pointer text-indigo-500"><a href="https://drive.google.com/file/d/1fQlv3_rat0iDbNGkFGdqivEqgBPn3n60/view" target="_blank"  rel="noreferrer">Resume</a></li>
+                <li className="p-2 mr-48 cursor-pointer text-indigo-500"><a href="https://drive.google.com/file/d/1tqgPSjMbsUq9txNaoHkJv9oWhHe3Nt4Q/view?usp=drive_link" target="_blank"  rel="noreferrer">Resume</a></li>
             </ul>
         </div>
     )
